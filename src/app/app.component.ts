ec82190c3b15aaeca57fd2a766fd1cd2444e705c
MisaghDayer <missaghd@gmail.com>
2020-11-07 12:43:12 +0330

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'srb-rms';
+export class AppComponent implements AfterViewInit {
+  submitting: boolean;
+  loggedIn: boolean;
+
+  dataSource = [
+    { name: 'Hi', value: 56},
+    { name: 'Hello', value: 853},
+    { name: 'Hello', value: 853},
+    { name: 'Hello', value: 853},
+    { name: 'Hello', value: 853},
+  ];
+
+  columnsToDisplay = [
+    '_row',
+    'name',
+    'value'
+  ];
+
+  items = [
+    {value: 'I can be dragged', disabled: false},
+    {value: 'I cannot be dragged', disabled: true},
+    {value: 'I can also be dragged', disabled: false}
+  ];
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    const date: Date = new Date();
+    console.log('Date = ' + date);
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -10,7 +24,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatSortModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
